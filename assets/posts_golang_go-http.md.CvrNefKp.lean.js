6a55466b18acae5e118db77ded5f648dd649283e
pzx521521 <pzx521521@users.noreply.github.com>
2024-11-07 08:03:51 +0000

    deploy: a6d4ff5c27ee92c318db78bd67b7e4955169d6cf

---
+++ assets/posts_golang_go-http.md.CvrNefKp.lean.js
@@ -0,0 +1,54 @@
+import{_ as s,c as a,a0 as p,o as e}from"./chunks/framework.DADtiyJ8.js";const d=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"posts/golang/go-http.md","filePath":"posts/golang/go-http.md"}'),t={name:"posts/golang/go-http.md"};function l(i,n,c,r,o,h){return e(),a("div",null,n[0]||(n[0]=[p(`<p>golang http 包源码简介(中间件实现原理/函数作为接口):</p><p>下面4段代码都可以开启一个http服务,他们有什么区别呢?</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>type myHandler struct {</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func (h myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {</span></span>
+<span class="line"><span>	io.WriteString(w, &quot;Hello from a HandleFunc #1!\\n&quot;)</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span>func http1() {</span></span>
+<span class="line"><span>	h := myHandler{}</span></span>
+<span class="line"><span>	log.Fatalln(http.ListenAndServe(&quot;:8000&quot;, h))</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func http2() {</span></span>
+<span class="line"><span>	h := myHandler{}</span></span>
+<span class="line"><span>	http.Handle(&quot;/hello&quot;, &amp;h)</span></span>
+<span class="line"><span>	log.Fatalln(http.ListenAndServe(&quot;:8000&quot;, nil))</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>var h = func(w http.ResponseWriter, r *http.Request) {</span></span>
+<span class="line"><span>	w.Write([]byte(&quot;hello&quot;))</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func http3() {</span></span>
+<span class="line"><span>	http.ListenAndServe(&quot;:8000&quot;, http.HandlerFunc(h))</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func http4() {</span></span>
+<span class="line"><span>	http.HandleFunc(&quot;/hello&quot;, h)</span></span>
+<span class="line"><span>	http.ListenAndServe(&quot;:8000&quot;, nil)</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>http2 相对 http1 多了 一个路径解析.</p><p>http1 和 http3 是什么区别呢?</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>type Handler interface {</span></span>
+<span class="line"><span>	ServeHTTP(ResponseWriter, *Request)</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>type HandlerFunc func(ResponseWriter, *Request)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>// ServeHTTP calls f(w, r).</span></span>
+<span class="line"><span>func (f HandlerFunc) ServeHTTP(w ResponseWriter, r *Request) {</span></span>
+<span class="line"><span>   f(w, r)</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>http1 一个是类(myHandler)实现了接口(Handler).</p><p>http3 一个是函数(HandlerFunc)也可以实现接口(Handler).</p><p>然后看一下 如何实现中间件:</p><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>func Wrapper(h http.Handler) http.Handler {</span></span>
+<span class="line"><span>	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {</span></span>
+<span class="line"><span>		w.Write([]byte(&quot;before wrap\\n&quot;))</span></span>
+<span class="line"><span>		h.ServeHTTP(w, r)</span></span>
+<span class="line"><span>		w.Write([]byte(&quot;after wrap\\n&quot;))</span></span>
+<span class="line"><span>	})</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func wrap1() {</span></span>
+<span class="line"><span>	h := myHandler{}</span></span>
+<span class="line"><span>	log.Fatalln(http.ListenAndServe(&quot;:8000&quot;, Wrapper(h)))</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func wrap2() {</span></span>
+<span class="line"><span>	http.ListenAndServe(&quot;:8000&quot;, Wrapper(http.HandlerFunc(h)))</span></span>
+<span class="line"><span>}</span></span></code></pre></div>`,10)]))}const g=s(t,[["render",l]]);export{d as __pageData,g as default};
---
+++ assets/posts_单片机_tcp46.md.C8B4q_ZI.lean.js
@@ -0,0 +1,43 @@
+import{_ as s,c as n,a0 as p,o as t}from"./chunks/framework.DADtiyJ8.js";const u=JSON.parse('{"title":"golang tcp6 网无法访问","description":"","frontmatter":{"title":"golang tcp6 网无法访问","hidemeta":true},"headers":[],"relativePath":"posts/单片机/tcp46.md","filePath":"posts/单片机/tcp46.md"}'),e={name:"posts/单片机/tcp46.md"};function l(i,a,o,c,r,h){return t(),n("div",null,a[0]||(a[0]=[p(`<h1 id="记一次golang服务外网无法访问的问题" tabindex="-1">记一次golang服务外网无法访问的问题 <a class="header-anchor" href="#记一次golang服务外网无法访问的问题" aria-label="Permalink to &quot;记一次golang服务外网无法访问的问题&quot;">​</a></h1><h2 id="tcp6导致的外网无法访问" tabindex="-1">TCP6导致的外网无法访问 <a class="header-anchor" href="#tcp6导致的外网无法访问" aria-label="Permalink to &quot;TCP6导致的外网无法访问&quot;">​</a></h2><p>golang 代码如下:</p><div class="language-golang vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">golang</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>package main</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>import (</span></span>
+<span class="line"><span>	&quot;io&quot;</span></span>
+<span class="line"><span>	&quot;log&quot;</span></span>
+<span class="line"><span>	&quot;net/http&quot;</span></span>
+<span class="line"><span>	&quot;time&quot;</span></span>
+<span class="line"><span>)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func HelloServer(w http.ResponseWriter, req *http.Request) {</span></span>
+<span class="line"><span>	io.WriteString(w, time.Now().String())</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func main() {</span></span>
+<span class="line"><span>	http.HandleFunc(&quot;/&quot;, HelloServer)</span></span>
+<span class="line"><span>	err := http.ListenAndServe(&quot;:8080&quot;, nil)</span></span>
+<span class="line"><span>	if err != nil {</span></span>
+<span class="line"><span>		log.Fatal(&quot;ListenAndServe: &quot;, err)</span></span>
+<span class="line"><span>	}</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>发现 用域名访问信息 <a href="https://xxxx:8080" target="_blank" rel="noreferrer">https://xxxx:8080</a> 无法显示</p><p>发现 用ip访问信息 {ip}:8080 无法显示</p><p>使用 netstat -antlp 之后发现 仅仅开了tcp6的端口而没有tcp的端口, 因此猜测是tcp6 导致的</p><p><img src="https://tva1.sinaimg.cn/large/006ulzy2ly1h4ukyatyx4j30o004e77q.jpg" alt="QQ截图20220804113739.png"></p><h2 id="解决方案1-修改tcp6为tcp4" tabindex="-1">解决方案1 修改tcp6为tcp4 <a class="header-anchor" href="#解决方案1-修改tcp6为tcp4" aria-label="Permalink to &quot;解决方案1 修改tcp6为tcp4&quot;">​</a></h2><p><a href="https://stackoverflow.com/questions/38592064/listen-on-tcp4-not-tcp6/38592286#38592286" target="_blank" rel="noreferrer">参考</a>, 修改代码如下:</p><div class="language-golang vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">golang</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>package main</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>import (</span></span>
+<span class="line"><span>	&quot;io&quot;</span></span>
+<span class="line"><span>	&quot;log&quot;</span></span>
+<span class="line"><span>	&quot;net&quot;</span></span>
+<span class="line"><span>	&quot;net/http&quot;</span></span>
+<span class="line"><span>	&quot;time&quot;</span></span>
+<span class="line"><span>)</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func HelloServer(w http.ResponseWriter, req *http.Request) {</span></span>
+<span class="line"><span>	io.WriteString(w, time.Now().String())</span></span>
+<span class="line"><span>}</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>func main() {</span></span>
+<span class="line"><span>	http.HandleFunc(&quot;/&quot;, HelloServer)</span></span>
+<span class="line"><span>	l, err := net.Listen(&quot;tcp4&quot;, &quot;:8080&quot;)</span></span>
+<span class="line"><span>	err = http.Serve(l, nil)</span></span>
+<span class="line"><span>	if err != nil {</span></span>
+<span class="line"><span>		log.Fatal(&quot;ListenAndServe: &quot;, err)</span></span>
+<span class="line"><span>	}</span></span>
+<span class="line"><span>}</span></span></code></pre></div><p>就可发现 tcp6的端口而没有tcp的端口(ps: 修改为 tcp 没有效果)</p><h2 id="解决方案2-centos设置tcp4转发tcp6" tabindex="-1">解决方案2 centos设置tcp4转发tcp6 <a class="header-anchor" href="#解决方案2-centos设置tcp4转发tcp6" aria-label="Permalink to &quot;解决方案2 centos设置tcp4转发tcp6&quot;">​</a></h2><p>上面的办法不是很好, 正确的方式应该是 开启ipv4端口转发</p><p><a href="https://blog.csdn.net/zhouzhou992/article/details/122697920" target="_blank" rel="noreferrer">参考</a></p><h3 id="临时设置-重启网卡或服务器后会丢失配置" tabindex="-1">临时设置(重启网卡或服务器后会丢失配置) <a class="header-anchor" href="#临时设置-重启网卡或服务器后会丢失配置" aria-label="Permalink to &quot;临时设置(重启网卡或服务器后会丢失配置)&quot;">​</a></h3><ol><li>检查是否开启ipv4端口转发</li></ol><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>sysctl net.ipv4.ip_forward</span></span></code></pre></div><ol start="2"><li>若指令返回结果为net.ipv4.ip_forward = 0，执行如下指令</li></ol><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>echo 1 &gt; /proc/sys/net/ipv4/ip_forward</span></span>
+<span class="line"><span>sysctl -w net.ipv4.ip_forward=1</span></span></code></pre></div><h3 id="永久设置" tabindex="-1">永久设置 <a class="header-anchor" href="#永久设置" aria-label="Permalink to &quot;永久设置&quot;">​</a></h3><p>修改/etc/sysctl.conf文件</p><p>添加或修改:</p><div class="language-sh vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">net.ipv4.ip_forward</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">=1</span></span></code></pre></div><p>加载配置</p><div class="language-sh vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">sh</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">sysctl</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;"> -p</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> /etc/sysctl.conf</span></span></code></pre></div><h3 id="检查" tabindex="-1">检查 <a class="header-anchor" href="#检查" aria-label="Permalink to &quot;检查&quot;">​</a></h3><p>完成后通过netstat -antlpu 仍旧显示只有tcp6，但是此时通过ipv4地址是可以访问了。</p><h2 id="cloudflare-导致的https-无法访问的问题" tabindex="-1">cloudflare 导致的https 无法访问的问题 <a class="header-anchor" href="#cloudflare-导致的https-无法访问的问题" aria-label="Permalink to &quot;cloudflare 导致的https 无法访问的问题&quot;">​</a></h2><p>但是 https 是无法访问的, 原因是 经过了cloudflare的CDN</p><p>只允许80 443 过 https, 不用cloudflare做ssl, 或者不用https 即可</p>`,31)]))}const g=s(e,[["render",l]]);export{u as __pageData,g as default};
